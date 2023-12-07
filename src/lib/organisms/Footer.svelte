6081a1bb9bf01cbe77de0959eea8450334f56964
Suuskehh <suus.harsveld@hva.nl>
2023-12-07 11:01:31 +0100

    Installation

---
+++ src/lib/organisms/Footer.svelte
@@ -0,0 +1,25 @@
+
+<script>
+
+	import {FooterContact, FooterLinks, FooterExtra} from '$lib/index.js'
+	
+</script>
+
+<footer> 
+
+    <div class="footer-container"></div>
+	<FooterLinks />
+	<FooterContact />
+	<FooterExtra />
+
+</footer>
+
+<style> 
+
+	footer{
+		background-color: #051a1a;
+		display: flex;
+		padding: 3em;	
+	}
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { MainHead, MainAbout, Stats,  } from "../lib/index.js";
+</script>
+
+<svelte:head>
+    <title>INK</title>
+</svelte:head>
+
+<MainHead />
+<MainAbout />
+<Stats />
