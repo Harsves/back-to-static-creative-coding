9827bbc17636c98e51905ad0c96628b46cc45a2c
Suuskehh <suus.harsveld@hva.nl>
2023-12-08 11:28:13 +0100

    Update

--- src/lib/atoms/Card.svelte
+++ src/lib/atoms/Card.svelte
@@ -1,6 +1,31 @@
 <section class="card">
+    <div class="name">
+        <span class="char1">N</span>
+        <span class="char2">e</span>
+        <span class="char3">t</span>
+        <span class="char4">o</span>
+        <span class="char5"></span>
+        <span class="char6">B</span>
+        <span class="char7">i</span>
+        <span class="char8">t</span>
+        <span class="char9">e</span>
+        <span class="char10">n</span>
+        <span class="char11">C</span>
+        <span class="char12">o</span>
+        <span class="char13">u</span>
+        <span class="char14">r</span>
+        <span class="char15">t</span>
+        <span class="char16"></span>
+        <span class="char17">M</span>
+        <span class="char18">e</span>
+        <span class="char19">n</span>
+        <span class="char20">d</span>
+        <span class="char21">e</span>
+        <span class="char22">s</span>
+    </div> 
     <img src="assets/Jongen1.png" alt="Neto BitenCourt Mendes">
-    <h1>Neto BitenCourt Mendes</h1>
+    <!-- <h1>Neto BitenCourt Mendes</h1> -->
+    
 </section>
 
 <style>
@@ -8,17 +33,115 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-image: url("assets/_7a0aff71-0981-45b0-a94b-89c0aa9d1ffc.jpeg");
+    background-image: url("assets/Background-card.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 380px;
 
 }
 
-h1{
+.name span {
     font-family: Retro;
-    font-size: 37px;
+    font-size: 23px;
     text-align: center;
+    color: #FF9800;
+    display: inline-block;
+    height: 240px;
+    max-width: 0px;
+    margin-top: 32px;
+    margin-bottom: -210px;
 }
 
 img {
-    max-width: 90%;
+    max-width: 40%;
+    margin-top: 50px;
 }
+ 
+.char1 {
+  transform: rotate(-60deg);
+}
+
+.char2 {
+  transform: rotate(-54deg);
+}
+
+.char3 {
+  transform: rotate(-48deg);
+}
+
+.char4 {
+  transform: rotate(-42deg);
+}
+
+.char5 {
+  transform: rotate(-36deg);
+}
+
+.char6 {
+  transform: rotate(-30deg);
+}
+
+.char7 {
+  transform: rotate(-24deg);
+}
+
+.char8 {
+  transform: rotate(-18deg);
+}
+
+.char9 {
+  transform: rotate(-12deg);
+}
+
+.char10 {
+  transform: rotate(-6deg);
+}
+
+.char11 {
+  transform: rotate(0deg);
+}
+.char12 {
+  transform: rotate(6deg);
+}
+
+.char12 {
+  transform: rotate(12deg);
+}
+
+.char14 {
+  transform: rotate(18deg);
+}
+
+.char15 {
+  transform: rotate(24deg);
+}
+
+.char16 {
+  transform: rotate(30deg);
+}
+
+.char17 {
+  transform: rotate(36deg);
+}
+
+.char18 {
+  transform: rotate(42deg);
+}
+
+.char19 {
+  transform: rotate(48deg);
+}
+
+.char20 {
+  transform: rotate(54deg);
+}
+
+.char21 {
+  transform: rotate(60deg);
+} 
+
+.char22 {
+  transform: rotate(66deg);
+} 
+
 </style>
